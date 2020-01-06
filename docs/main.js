dfed9b09349954ee86f76072c8cda69cc534857e
dojo <sharadcodes>
2020-01-06 13:43:31 +0530

    up

---
+++ docs/main.js
@@ -0,0 +1,9 @@
+document.getElementById("stdgrp").addEventListener("click", function() {
+  document.getElementById("p-chat").classList.toggle("show");
+});
+
+
+document.getElementById("back_img").addEventListener("click", function() {
+    document.getElementById("p-chat").classList.toggle("show");
+  });
+  
